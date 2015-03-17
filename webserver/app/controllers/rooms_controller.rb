2bbb305abcdcd0133a87cd7a8c869ef662029ba0
Kenny Ruddick <ruddick1@tcnj.edu>
2015-03-17 16:17:30 -0400

    Create rooms, show rooms, list rooms
    Password protected rooms

--- webserver/app/controllers/rooms_controller.rb
+++ webserver/app/controllers/rooms_controller.rb
@@ -1,12 +1,44 @@
 class RoomsController < ApplicationController
-  def initialize
-    @error = "Could not find the specified room. Try going to http://localhost:3000/rooms/public/edit"
+
+  before_action :authenticate, only: [:show, :edit]
+
+  def index
+    @rooms = Room.all
   end
 
   def new
   end
 
-  def edit
-    render plain: @error if params[:id] != "public"
+  def create
+    @room = Room.new(room_params)
+
+    @room.save
+    redirect_to @room
   end
+
+  def edit
+  end
+
+  def show
+    if params[:id] == "public"
+      @room = Room.find(1)
+    else
+      @room = Room.find(params[:id])
+    end
+  end
+
+  private
+
+    def authenticate
+      if params[:id] != "public"
+        authenticate_or_request_with_http_basic do |username, password|
+          @username = username
+          password == Room.find(params[:id]).password
+        end
+      end
+    end
+
+    def room_params
+      params.require(:room).permit(:name, :password)
+    end
 end
