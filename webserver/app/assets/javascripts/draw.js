1a6baf9bb1551306b474d6b02a7004f2a59d7ceb
Curran Prasad <Curran@currans-mbp.home>
2015-04-05 14:21:41 -0400

    Added drawing functionalities (minus textbox, zoom), full colorset

--- webserver/app/assets/javascripts/draw.js
+++ webserver/app/assets/javascripts/draw.js
@@ -1,14 +1,7 @@
-/*
-* Enables the user to draw on the canvas
-* 3/12/15
-* Multiple colors and tools have not been implemented
-* Basic groundwork for the UI is laid- will refine it a good deal
-* Will incorporate a 'clear canvas' function very soon
-*/
-
+window.addEventListener("keydown", checkifE, false);
+window.addEventListener("keydown", checkifB, false);
+window.addEventListener("keydown", checkifC, false)
 var context;
-
-
 var mouseClicked = false;
 
 // Previous frame's mouse position
@@ -17,9 +10,40 @@ var recentY;
 
 var drawColor = "rgb(0, 0, 0)";
 
+var lineWidth = 5;
+
 function initialize() {
     context = document.getElementById("frameCanvas").getContext("2d");
 
+    $("#pencil").click(function(){
+
+        lineWidth = 2;
+    });
+
+    $("#eraser").click(function(){
+        drawColor = "white";
+        alert("Press 'e' to modify eraser size, 'c' to clear frame");
+
+    });
+
+    $("#brush").click(function(){
+        lineWidth = 15;
+        alert("Press 'b' to modify brush size");
+
+    });
+
+    $("#textBox").click(function(){
+
+        // Mouse pressed, create text area on mouse click of arbitrary (small) size
+        // User can expand text area anyways
+
+    });
+
+    $("#bucket").click(function(){
+        context.fillStyle = drawColor;
+        context.fillRect(0,0,780,540);
+
+    });
     /*
     * Mouse input events
     */
@@ -61,7 +85,7 @@ function sketch(x, y, drawing) {
     if(drawing) {
         context.beginPath();
         context.strokeStyle = drawColor;
-        context.lineWidth = 5;
+        context.lineWidth = lineWidth;
         context.moveTo(recentX, recentY);
         context.lineTo(x, y);
         context.closePath();
@@ -70,3 +94,30 @@ function sketch(x, y, drawing) {
     recentX = x;
     recentY = y;
 }
+
+function checkifE(e) {
+    if (e.keyCode == "69") {
+        lineWidth = prompt("Enter size of eraser: ");
+        drawColor = "white";
+    }
+}
+
+function checkifB(e) {
+    if (e.keyCode == "66"){
+        lineWidth = prompt("Enter size of brush: ");
+    }
+}
+
+function checkifC(e) {
+
+    if (e.keyCode == "67"){
+        context.clearRect(0, 0, 780, 540);
+    }
+}
+
+
+
+
+
+
+
