67fc2c60789696aaf7952e47cf055a473e425427
Kenny Ruddick <ruddick1@tcnj.edu>
2015-04-12 17:46:37 -0400

    Credentials configuration fix

--- webserver/config/initializers/creds.rb
+++ webserver/config/initializers/creds.rb
@@ -2,5 +2,5 @@ require 'yaml'
 
 # Loads the decrypted credentials file into a global constant
 creds = YAML.load_file(Rails.root.join('config', 'creds.yml'))
-Rails.configuration.db_username = creds["db"]["username"]
-Rails.configuration.db_password = creds["db"]["password"]
+Rails.application.config.db_username = creds["db"]["username"]
+Rails.application.config.db_password = creds["db"]["password"]
