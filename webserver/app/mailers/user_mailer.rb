9d1409e04712dd2499e5456a434870ba5653e434
mcnallm2 <mcnallm2@tcnj.edu>
2015-04-13 18:35:03 -0400

    Update user_mailer.rb

--- webserver/app/mailers/user_mailer.rb
+++ webserver/app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@
       # Make sure to set this to your verified sender!
       default from: "mcnallm2@tcnj.edu"  
 
-      def test(email)
+      def server_email(email)
         mail(:to => email, :subject => "Hello World!")
       end
     end 
