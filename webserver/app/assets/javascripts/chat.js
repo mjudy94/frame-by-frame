51843dd9775cdf2a7a2442d011756fc1c14147a7
Kenny Ruddick <ruddick1@tcnj.edu>
2015-04-30 01:20:07 -0400

    Add name labels to chat messages
    Previously the chat messages were just sent by themselves and it
    was impossible to determine who said what. Now messages have a
    label which indicate the sender. Messages from yourself show up as
    the label "Me".

--- webserver/app/assets/javascripts/chat.js
+++ webserver/app/assets/javascripts/chat.js
@@ -1,25 +1,67 @@
 $(function() {
-  var client = new Faye.Client("http://localhost:9292/faye");
-  var form = $(".chat").find("form");
-  var roomId = form.data('id');
-  var password = form.data('password');
-  var channel = "/chat/" + roomId + "p" + password;
+  var client = new Faye.Client("http://localhost:9292/faye"),
+      chatWidget = $(".chat"),
+      greeting = chatWidget.find(".greeting"),
+      messageForm = chatWidget.find(".messageForm"),
+      messageBox = chatWidget.find(".messages"),
+      nameForm = chatWidget.find('.nameForm'),
+      nameInput = nameForm.find("input:first");
+      roomId = messageForm.data('id'),
+      password = messageForm.data('password'),
+      channel = "/chat/" + roomId + "p" + password,
+      username = localStorage.getItem('username') || 'Guest',
+      userId = guid();
 
+  // MESSAGE SUBSCRIBE
   client.subscribe(channel, function(data) {
-    var messageBox = $(".chat").find(".messages");
-    messageBox.append("<p>" + data.msg);
+    var isOwnMessage = data.userId === userId,
+        className =  isOwnMessage ? 'self' : 'other',
+        name = isOwnMessage ? 'Me' : data.from;
+    messageBox.append("<p><strong class='" + className + "'>" + name +
+        ":</strong> " + data.msg);
     messageBox.animate({
       scrollTop: messageBox.prop("scrollHeight")
     }, "slow");
   });
 
-
-  form.submit(function() {
-    var input = $("input:first");
+  // MESSAGE INPUT AND PUBLISH
+  messageForm.submit(function() {
+    var input = messageForm.find("input:first");
     client.publish(channel, {
-      msg: input.val()
+      msg: input.val(),
+      from: username,
+      userId: userId
     });
     input.val("");
     return false;
   });
+
+  // NAME DIALOG FORM
+  nameForm.dialog({
+    title: 'Change your display name',
+    autoOpen: false,
+    modal: true,
+    width: 350,
+    open: function() {
+      nameInput.val(username).select();
+    }
+  });
+
+  nameForm.submit(function() {
+    nameForm.dialog("close");
+    username = nameInput.val();
+    localStorage.setItem("username", username);
+    updateGreeting();
+    return false;
+  });
+
+  chatWidget.find('button').click(function() {
+    nameForm.dialog("open");
+  });
+
+  function updateGreeting() {
+    greeting.text(username);
+  };
+
+  updateGreeting();
 });
