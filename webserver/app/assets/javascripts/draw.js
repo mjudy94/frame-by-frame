7af02673f3c644a95e4510e85b19ba319a8460d3
Kenny Ruddick <ruddick1@tcnj.edu>
2015-05-10 23:42:27 -0400

    Implement basic drawing functions in SVG rather than canvas
    - Include Snap.svg to dynamically generate and interact with SVG
    elements
    - Implement "sketch" function
    - Implement "clear" function
    - Minor optimization improvements

--- webserver/app/assets/javascripts/draw.js
+++ webserver/app/assets/javascripts/draw.js
@@ -1,7 +1,11 @@
+//= require snap-svg
+
 (function() {
-  var canvas, context;
+  var MIN_LINE_LENGTH = 5;
+
+  var svg, svgElement, publishAction;
   var mouseClicked = false;
-  
+
   // Previous frame's mouse position
   var recentX;
   var recentY;
@@ -32,28 +36,35 @@
 
       client.subscribe(channel, function(data) {
         var isOwnSketchAction = data.userId === userId,
-            className = isOwnSketchAction ? 'self' : 'other',
-            name = isOwnSketchAction ? 'Me' : data.fromUser;
+            className = isOwnSketchAction ? 'self' : 'other';
 
         //Add isOwnSketchAction as param so sketch knows whether or not to ignore the following sketch request?
         if(!isOwnSketchAction) {
-          if(data.action === "sketch") {
-            sketch(data.guestX, data.guestY, data.guestDrawing, data.guestRecentX, data.guestRecentY, data.guestLineWidth, data.guestDrawColor, isOwnSketchAction);
-          } else if(data.action === "fill") {
-            fill(data.guestDrawColor, isOwnSketchAction);
-          } else if(data.action === "clear") {
-            clear(isOwnSketchAction);
+          // if(data.action === "sketch") {
+          //   sketch(data.guestX, data.guestY, data.guestDrawing, data.guestRecentX, data.guestRecentY, data.guestLineWidth, data.guestDrawColor, isOwnSketchAction);
+          // } else if(data.action === "fill") {
+          //   fill(data.guestDrawColor, isOwnSketchAction);
+          // } else if(data.action === "clear") {
+          //   clear(isOwnSketchAction);
+          // }
+          switch (data.action) {
+            case "sketch":
+              svg.add(Snap.parse(data.svg));
+              break;
+            case "clear":
+              svg.clear();
+              break;
+            default:
+              break;
           }
         }
       });
-    
-      canvas = document.getElementById("canvas");
-      if (!canvas) {
-        // Stop execution if the canvas is not found
+
+      svg = Snap("#canvas");
+      if (!svg) {
+        // Stop execution if the svg canvas is not found
         return;
       }
-
-      context = canvas.getContext("2d");
 
       // Hotkey binding
       $(document).bind("keydown", "c", function() { clear(true); });
@@ -76,13 +87,12 @@
 
       $("#clear").click(function(){
          clear(true);
- 
+
       });
 
       $(".tool").click(function(){
         $(".tool").not($(this)).css("border", "none");
         $(this).css("border", "2px solid black");
-
       });
 
       /*
@@ -123,21 +133,17 @@
         }
       }, false);
 
-      var inputStopFunc = function(e) {
-        mouseClicked = false;
-      };
-      
-      //triggers when user removes finger wnile within bounds of specified element
-      $("#canvas")[0].addEventListener('touchend', inputStopFunc);
+      //triggers when user removes finger while within bounds of specified element
+      $("#canvas")[0].addEventListener('touchend', commitInput);
 
       // Mouse un-press
-      $("#canvas").mouseup(inputStopFunc);
+      $("#canvas").mouseup(commitInput);
 
       //triggers when no longer touching the canvas
-      $("#canvas")[0].addEventListener('touchleave', inputStopFunc);
+      $("#canvas")[0].addEventListener('touchleave', commitInput);
 
       // Mouse goes off the canvas
-      $("#canvas").mouseleave(inputStopFunc);
+      $("#canvas").mouseleave(commitInput);
 
 
       // Color pickers
@@ -156,31 +162,58 @@
       });
   });
 
-  function sketch(x, y, drawing, rX, rY, lwidth, dcolor, isOwnSketch) {
+  function commitInput() {
+    if (publishAction) {
+      client.publish(channel, {
+        userId: userId,
+        svg:  svgElement && svgElement.toString(),
+        action: publishAction
+      });
+    }
+
+    mouseClicked = false;
+    svgElement = null;
+    publishAction = null;
+  };
+
+  function sketch(x, y, drawing, rx, ry, lwidth, dcolor, isOwnSketch) {
     if(channel) {
       if(drawing) {
-        context.beginPath();
-        context.strokeStyle = dcolor;
-        context.lineWidth = lwidth;
-        context.moveTo(rX, rY);
-        context.lineTo(x, y);
-        context.lineCap = 'round';
-        context.stroke();
+        if (Math.abs(x - rx) < MIN_LINE_LENGTH  &&
+              Math.abs(y - ry) < MIN_LINE_LENGTH) {
+          // Do not draw anything if the change is too insignificant. This helps
+          // conserve bandwidth and size of the svg image.
+          return;
+        }
+
+        publishAction = "sketch";
+
+        if (!svgElement) {
+          svgElement = svg.polyline(x, y, rx, ry).attr({
+            "stroke": dcolor,
+            "stroke-width": lwidth,
+            "stroke-linecap": "round",
+            "stroke-linejoin": "round",
+            "fill": "none"
+          });
+        } else {
+          svgElement.attr("points", svgElement.attr("points").concat(rx, ry))
+        }
 
         //send faye message here with x, y, drawing, recentX, recentY, drawColor, lineWidth
-        if(isOwnSketch) {
-          client.publish(channel, {
-            userId: userId,
-            action: "sketch",
-            guestX: x,
-            guestY: y,
-            guestDrawing: drawing,
-            guestRecentX: rX,
-            guestRecentY: rY,
-            guestDrawColor: dcolor,
-            guestLineWidth: lwidth
-          });
-        }
+        // if(isOwnSketch) {
+        //   client.publish(channel, {
+        //     userId: userId,
+        //     action: "sketch",
+        //     guestX: x,
+        //     guestY: y,
+        //     guestDrawing: drawing,
+        //     guestRecentX: rx,
+        //     guestRecentY: ry,
+        //     guestDrawColor: dcolor,
+        //     guestLineWidth: lwidth
+        //   });
+        // }
       }
 
       if (isOwnSketch) {
@@ -206,12 +239,10 @@
 
   function clear(isOwnSketch) {
     if(channel) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      svg.clear();
       if(isOwnSketch) {
-        client.publish(channel, {
-          userId: userId,
-          action: "clear"
-        });
+        publishAction = "clear";
+        commitInput();
       }
     }
   }
@@ -224,7 +255,4 @@
   function resizeBrush() {
     lineWidth = prompt("Enter size of brush: ");
   }
-
-  
-
 })();
